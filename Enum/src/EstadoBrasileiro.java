cfb6c06eb520ac16b5ddd875a1c2ea72f9ebfaed
Luiz1405 <kubaszewski001@gmail.com>
2024-06-05 20:58:04 -0300

    Enum

---
+++ Enum/src/EstadoBrasileiro.java
@@ -0,0 +1,26 @@
+public enum EstadoBrasileiro {
+    SAO_PAULO ("SP", "São Paulo"),
+    RIO_DE_JANEIRO ("RJ", "Rio de Janeiro"),
+    PIAUI ("PI", "Piauí"),
+    MARANHAO ("MA", "Maranhão"),
+    CEARA ("CE", "Ceará"),
+    ;
+
+    private String nome;
+    private String sigla;
+
+    private EstadoBrasileiro(String nome, String sigla) {
+        this.nome = nome;
+        this.sigla = sigla;
+    }
+    public String getSigla(){
+        return sigla;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public String getNomeMaiusculo(){
+        return nome.toUpperCase();
+    }
+}
