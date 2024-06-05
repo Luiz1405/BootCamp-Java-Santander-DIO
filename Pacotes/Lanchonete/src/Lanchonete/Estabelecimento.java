9606c9d9d01e183e5757d7b23174973c2f56b6ed
Luiz1405 <kubaszewski001@gmail.com>
2024-06-05 19:26:55 -0300

    Visibilidade dos recuros

---
+++ Pacotes/Lanchonete/src/Lanchonete/Estabelecimento.java
@@ -0,0 +1,27 @@
+package Lanchonete;
+
+import Lanchonete.Atendimento.Atendente;
+
+public class Estabelecimento {
+	public static void main(String[] args) {
+		Cozinheiro cozinheiro = new Cozinheiro();
+		//ações que estabelecimento precisa ter ciência
+		cozinheiro.adicionarSucoNoBalcao();;
+		cozinheiro.adicionarLancheNoBalcao();
+		cozinheiro.adicionarComboNoBalcao();
+		
+		
+		Atendente atendente = new Atendente();
+		atendente.receberPagamento();
+		atendente.servindoMesa();
+		
+		
+		Cliente cliente = new Cliente();
+		cliente.escolherLanche();
+		cliente.fazerPedido();
+		cliente.pagarConta();
+		
+		
+		
+	}
+}
